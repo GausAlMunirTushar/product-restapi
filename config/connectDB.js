c20ba86162cf0bdc242f2d5dd88b5132bbb2e4b5
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-02-19 22:08:18 +0600

    dot env variable change

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const MONGODB_URI = process.env.MONGO_URI || 'mongodb+srv://admin:tushar@cluster0.bstsr.mongodb.net/product-api';
+const MONGODB_URI = 'mongodb+srv://admin:tushar@cluster0.bstsr.mongodb.net/product-api';
 
 mongoose.set('strictQuery', false);
 
