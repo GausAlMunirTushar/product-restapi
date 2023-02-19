bc2dee3beef70a365de4a0e3fd90bde8ccdb261d
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-02-19 21:59:01 +0600

    env variable

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/products-api';
+require('dotenv').config();
+const MONGODB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/products-api';
 
 mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(MONGODB_URI);
         console.log('MongoDB connected');
     }
     catch(err){
